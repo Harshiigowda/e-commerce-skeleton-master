e0a4af26ad3560a969c8416f84a5947538f53a28
Harshitha A <harshithaa916@gmail.com>
2024-05-31 12:09:34 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <img className='about' src='https://t3.ftcdn.net/jpg/02/41/43/18/360_F_241431868_8DFQpCcmpEPVG0UvopdztOAd4a6Rqsoo.jpg'></img>
+      <h2>Shopping is an essential aspect of life as it allows individuals to purchase goods and services that they need for daily living. It also provides a means of fulfilling wants and desires, as well as keeping up with current trends and styles.</h2>
+
+      <h2>Regular shopping is important as it allows individuals to replenish necessary items such as groceries, household supplies, and personal care products. It also allows for the purchase of new clothing, technology, and other items that may enhance one's quality of life.</h2>
+
+      <h2>In conclusion, shopping is an important aspect of life that allows individuals to meet their basic needs and fulfill their desires. It can also have a positive impact on mental health when done responsibly.</h2>
+    
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to our Shopping</h1>
+      <img className='img' src="https://watermark.lovepik.com/photo/40008/0007.jpg_wh1200.jpg"></img>
+    </div>
+  );
+}
+
+export default Home;
